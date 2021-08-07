d74639dcf964252a2bb2b1dd7c1c9d827e729033
Mr. Unknown <noom1009@gmail.com>
2021-08-07 20:21:01 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,139 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import "./App.css";
+//import * as tf from "@tensorflow/tfjs";
+import * as facemesh from "@tensorflow-models/face-landmarks-detection";
+import * as handpose from "@tensorflow-models/handpose";
+//import cocossd from'@tensorflow-models/coco-ssd';
+import backend from '@tensorflow/tfjs-backend-webgl';
+import '@tensorflow/tfjs-backend-webgl';
+import Webcam from "react-webcam";
+import { drawMesh } from "./utilities";
+import { drawHand } from "./utilitiesHand";
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  const runHandpose = async () => {
+    const net = await handpose.load();
+    console.log("Handpose model loaded.");
+    setInterval(() => {
+      detectHandpose(net);
+    }, 100);
+  };
+
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+      const face = await net.estimateFaces({ input: video });
+      console.log(face);
+      const ctx = canvasRef.current.getContext("2d");
+      requestAnimationFrame(() => {
+        drawMesh(face, ctx);
+      });
+    }
+  };
+  useEffect(() => {
+    const runFacemesh = async () => {
+      const net = await facemesh.load(
+        facemesh.SupportedPackages.mediapipeFacemesh
+      );
+      setInterval(() => {
+        detect(net);
+      }, 10);
+    };
+    runFacemesh();
+  }, []);
+
+  const detectHandpose = async (net) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+      const hand = await net.estimateHands(video);
+      console.log(hand);
+      const ctx = canvasRef.current.getContext("2d");
+      drawHand(hand, ctx);
+    }
+  };
+
+  runHandpose();
+/*
+  const detectCoco = async (net) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+    }
+  };
+
+  useEffect(() => {
+    const runCoco = async () => {
+      const net = await cocossd.load();
+      setInterval(() => {
+        detectCoco(net);
+      }, 10);
+    };
+    runCoco();
+  }, []);
+*/
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
       </header>
     </div>
   );
